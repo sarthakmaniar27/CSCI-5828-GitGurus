4560616a7149433014d1e937a02eaa0118654694
Shanthi-Lekkala <shanthi.lekkala@colorado.edu>
2023-03-22 11:56:51 -0600

    Basic login app

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'AppSecretKey'     
+
+user = {"username": "admin", "password": "password"}
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if(request.method == 'POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')     
+        if username == user['username'] and password == user['password']:
+            
+            session['user'] = username
+            return redirect('/dashboard')
+
+        return "<h1>Wrong username or password</h1>"   
+
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    if('user' in session and session['user'] == user['username']):
+        return '<h1>Upcoming Project for Crime Report</h1>'
+
+    return '<h1>You are not logged in.</h1>'
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/login')
+
+if __name__ == '__main__':
+    app.run(debug=True)
