856960dea3a8c3255eed1e6649aeec22fcfc6728
= <7rishi20ss@gmail.com>
2023-04-19 18:29:04 -0600

    adding bar chart in main-card

--- Scripts/dashborad.js
+++ Scripts/dashborad.js
@@ -18,3 +18,30 @@ function closeSidebar(){
         sidebarOpen = false;
     }
 }
+
+var barChartOptions = {
+    series: [{
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350,
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 4,
+      horizontal: true,
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  xaxis: {
+    categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
+      'United States', 'China', 'Germany'
+    ],
+  }
+  };
+
+  var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+  barChart.render();
