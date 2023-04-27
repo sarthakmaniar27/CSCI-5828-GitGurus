faa3bbc4e135a57ef9e2e458364ee0a2af88d95e
Kunal Rane <kunal.sr@somaiya.edu>
2023-04-27 09:06:36 +0530

    added route for important contacts

--- app.py
+++ app.py
@@ -164,5 +164,9 @@ def logout():
 def about():
     return render_template('about.html')
 
+@app.route('/contacts')
+def contacts():
+    return render_template('imp_contacts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
